9cb5f9178ad3639c3acde74fc12f497d9010933f
William Lemaire <william.lemaire@usherbrooke.ca>
2022-12-03 22:10:50 +0100

    Added Meson build system

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ac_types/include/ac_int.h"
+//
+// main is where all program execution starts
+//
+int main(int argc, char **argv) {
+  printf("Hello there.\n");
+  return 0;
+}
